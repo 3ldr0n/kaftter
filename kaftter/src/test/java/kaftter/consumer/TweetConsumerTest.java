af1e17a1ddf94cbe64656e82a88c52a4df2db3ec
Edison Neto <ednetoali@gmail.com>
2020-06-28 17:03:47 -0300

    #15 Update consumer tests
    Validate manual acknowledment and exception cases.

--- kaftter/src/test/java/kaftter/consumer/TweetConsumerTest.java
+++ kaftter/src/test/java/kaftter/consumer/TweetConsumerTest.java
@@ -1,17 +1,18 @@
 package kaftter.consumer;
 
-import kaftter.exception.InvalidPayloadException;
 import kaftter.service.TweetService;
 import kaftter.tweet.Tweet;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.kafka.support.Acknowledgment;
 
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
@@ -26,20 +27,35 @@ public class TweetConsumerTest {
     @Test
     public void consumer_valid_message_should_save_to_database() {
         final var payload = new Tweet();
+        final var consumerRecord = new ConsumerRecord<>("", 0, 0, 0L, payload);
+        final var acknowledgment = mock(Acknowledgment.class);
         doNothing().when(tweetService).save(payload);
 
-        tweetConsumer.consume(payload);
+        tweetConsumer.consume(consumerRecord, acknowledgment);
 
         verify(tweetService).save(any(Tweet.class));
+        verify(acknowledgment).acknowledge();
     }
 
     @Test
-    public void consume_null_message_should_throw_exception() {
-        assertThatExceptionOfType(InvalidPayloadException.class).isThrownBy(() ->
-                tweetConsumer.consume(null)
-        );
+    public void consume_null_consumer_record_should_acknowledge_message_and_not_save() {
+        final var acknowledgment = mock(Acknowledgment.class);
+
+        tweetConsumer.consume(null, acknowledgment);
 
         verify(tweetService, never()).save(any(Tweet.class));
+        verify(acknowledgment).acknowledge();
+    }
+
+    @Test
+    public void consume_null_message_on_consumer_record_should_acknowledge_message_and_not_save() {
+        final var acknowledgment = mock(Acknowledgment.class);
+        final var consumerRecord = new ConsumerRecord<Long, Tweet>("", 0, 0, 0L, null);
+
+        tweetConsumer.consume(consumerRecord, acknowledgment);
+
+        verify(tweetService, never()).save(any(Tweet.class));
+        verify(acknowledgment).acknowledge();
     }
 
 }
