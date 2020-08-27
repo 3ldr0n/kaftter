187ab840a0cea0613d327beacdfc45f487087edc
Edison Neto <ednetoali@gmail.com>
2020-08-27 00:00:43 -0300

    #19 Remove API from kaftter consumer

--- kaftter/src/main/java/kaftter/service/TweetService.java
+++ kaftter/src/main/java/kaftter/service/TweetService.java
@@ -1,17 +1,9 @@
 package kaftter.service;
 
 import kaftter.converter.TweetEntityConverter;
-import kaftter.domain.TweetEntity;
 import kaftter.repository.TweetRepository;
-import kaftter.vo.Tweet;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.cassandra.core.query.CassandraPageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
-
-import java.util.LinkedList;
-import java.util.List;
 
 @Slf4j
 @Service
@@ -20,21 +12,6 @@ public class TweetService {
 
     public TweetService(final TweetRepository tweetRepository) {
         this.tweetRepository = tweetRepository;
-    }
-
-    /**
-     * Find certain number of tweets.
-     *
-     * @param numberOfTweets number of tweets to be fetched.
-     * @return A list of tweets.
-     */
-    public List<Tweet> findTweets(final int numberOfTweets) {
-        final Pageable pages = CassandraPageRequest.of(0, numberOfTweets);
-        final Slice<TweetEntity> tweetEntities = tweetRepository.findAll(pages);
-
-        final List<Tweet> tweets = new LinkedList<>();
-        tweetEntities.forEach(tweet -> tweets.add(tweet.toValue()));
-        return tweets;
     }
 
     public void save(final kaftter.tweet.Tweet tweet) {
